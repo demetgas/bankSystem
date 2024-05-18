3f20cebbde98a7546ca8141074bcf3e617a4d627
Demet Gaş <demet_gas@hotmail.com>
2024-05-18 19:11:26 +0200

    Update UserService.java

--- src/main/java/com/demetgas/bankSystem/service/UserService.java
+++ src/main/java/com/demetgas/bankSystem/service/UserService.java
@@ -1,4 +1,9 @@
 package com.demetgas.bankSystem.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class UserService {
 }
