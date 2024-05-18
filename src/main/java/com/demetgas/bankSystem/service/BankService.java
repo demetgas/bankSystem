5c9091fade4e7b46844500f51a2ea29ba0a50084
Demet Gaş <demet_gas@hotmail.com>
2024-05-18 21:28:07 +0200

    Update BankService.java

--- src/main/java/com/demetgas/bankSystem/service/BankService.java
+++ src/main/java/com/demetgas/bankSystem/service/BankService.java
@@ -1,11 +1,31 @@
 package com.demetgas.bankSystem.service;
 
+import com.demetgas.bankSystem.model.Bank;
+import com.demetgas.bankSystem.repository.BankRep;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bank")
 @AllArgsConstructor
 public class BankService {
+    private BankRep bankRep;
+
+    public List<Bank> getBanks(){
+        return bankRep.findAll();
+    }
+
+    public Bank getBankById(String id){
+        return bankRep.findById(id)
+                .orElseThrow(()-> new RuntimeException("Bank not found!"));
+    }
+
+    public Bank createBank(Bank newBank){
+        return bankRep.save(newBank);
+    }
+
+
 }
