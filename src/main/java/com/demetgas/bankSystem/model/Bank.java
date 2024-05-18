6a9c5d8b1ec102e0a061b9cf36d8513beaad6926
Demet Gaş <demet_gas@hotmail.com>
2024-05-18 19:07:35 +0200

    Update Bank.java

--- src/main/java/com/demetgas/bankSystem/model/Bank.java
+++ src/main/java/com/demetgas/bankSystem/model/Bank.java
@@ -1,4 +1,20 @@
 package com.demetgas.bankSystem.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bank {
+    private String bankName;
+    private List<Account> accountList = new ArrayList<>();
+    private double totalTransactionFeeAmount;
+    private double totalTransferAmount;
+    private double transactionFlatFeeAmount;
+    private double transactionPercentFeeValue;
 }
