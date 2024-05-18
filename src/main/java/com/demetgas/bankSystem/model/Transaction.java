fa0f2f4355f203a3bdde4cce52a472ee68ab2f24
Demet Gaş <demet_gas@hotmail.com>
2024-05-18 19:08:01 +0200

    Update Transaction.java

--- src/main/java/com/demetgas/bankSystem/model/Transaction.java
+++ src/main/java/com/demetgas/bankSystem/model/Transaction.java
@@ -1,4 +1,15 @@
 package com.demetgas.bankSystem.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
+    private double amount;
+    private String originatingAccountId;
+    private String resultingAccountId;
+    private String transactionReason;
 }
