58a0d46ac5d17047b3ae1b7fcf80b2cbd8345a18
Demet Gaş <demet_gas@hotmail.com>
2024-05-18 19:15:25 +0200

    Update AccountService.java

--- src/main/java/com/demetgas/bankSystem/service/AccountService.java
+++ src/main/java/com/demetgas/bankSystem/service/AccountService.java
@@ -1,12 +1,18 @@
 package com.demetgas.bankSystem.service;
 
+import com.demetgas.bankSystem.model.Account;
 import com.demetgas.bankSystem.repository.AccountRep;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AccountService {
     private AccountRep accountRep;
-    
+    public List<Account> getAccounts(){
+        return accountRep.findAll();
+    }
+
 }
