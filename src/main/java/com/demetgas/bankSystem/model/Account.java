28ddf901d83660445a3704ba3c91f7f8b02e37b6
Demet Gaş <demet_gas@hotmail.com>
2024-05-18 19:06:40 +0200

    Update Account.java

--- src/main/java/com/demetgas/bankSystem/model/Account.java
+++ src/main/java/com/demetgas/bankSystem/model/Account.java
@@ -1,4 +1,16 @@
 package com.demetgas.bankSystem.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
+    @Id
+    private String accountID;
+    private String username;
+    private double accountBalance;
 }
